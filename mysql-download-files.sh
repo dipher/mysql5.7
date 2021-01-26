3c581eb1da80e4fc8409755634acf13001ac21f6
dipher <35831983+dipher@users.noreply.github.com>
2021-01-26 18:41:06 +0800

    Update mysql-download-files.sh

--- mysql-download-files.sh
+++ mysql-download-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.33-linux-glibc2.12-x86_64.tar
+wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.33-linux-glibc2.12-x86_64.tar -O /tmp/mysql-5.7.33-linux-glibc2.12-x86_64.tar
 
-tar xvf mysql-5.7.33-linux-glibc2.12-x86_64.tar
+tar xvf /tmp/mysql-5.7.33-linux-glibc2.12-x86_64.tar -C /tmp/
  
