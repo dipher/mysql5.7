e0870c33d52e67b9a6ac911e070370f315705f87
dipher <35831983+dipher@users.noreply.github.com>
2021-01-28 09:53:00 +0800

    Update mysql-download-files.sh

--- mysql-download-files.sh
+++ mysql-download-files.sh
@@ -6,7 +6,7 @@ else
     wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.33-linux-glibc2.12-x86_64.tar -O /tmp/mysql-5.7.33-linux-glibc2.12-x86_64.tar
 fi
 
-if [ test -e "/tmp/mysql-5.7.33-linux-glibc2.12-x86_64.tar.gz"]; then
+if [ test -e "/tmp/mysql-5.7.33-linux-glibc2.12-x86_64.tar.gz" ]; then
     echo "The File exists,/tmp/mysql-5.7.33-linux-glibc2.12-x86_64.tar.gz"
 else
     tar xvf /tmp/mysql-5.7.33-linux-glibc2.12-x86_64.tar -C /tmp/
